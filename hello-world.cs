649ace603105705597cd90fe72b606efdb016a1e
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2026-02-10 20:00:43 +0300

    Day of learning

---
+++ hello-world.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "salary";
+            Console.WriteLine("Hello, World!!"+x);
+        }
+    }
+}
